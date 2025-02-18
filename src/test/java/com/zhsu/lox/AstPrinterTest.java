67f314d8519c185fc677c15e2a4a5d98d0dc665a
zhsu <szh12345szh@163.com>
2025-02-19 00:02:37 +0800

    add AsrPrinter

---
+++ src/test/java/com/zhsu/lox/AstPrinterTest.java
@@ -0,0 +1,19 @@
+package com.zhsu.lox;
+
+import org.junit.jupiter.api.Test;
+
+public class AstPrinterTest {
+
+    @Test
+    public void testAstPrinter() {
+        Expr expression = new Expr.Binary(
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
+
+        System.out.println(new AstPrinter().print(expression));
+    }
+}
