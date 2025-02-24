5c18ef36bae17e35e08ae240fb6f9387aae530a3
zhsu <szh12345szh@163.com>
2025-02-24 23:05:18 +0800

    report deviding zero error

--- src/main/java/com/zhsu/lox/Interpreter.java
+++ src/main/java/com/zhsu/lox/Interpreter.java
@@ -85,6 +85,9 @@ class Interpreter implements Expr.Visitor<Object> {
             }
             case SLASH -> {
                 checkNumberOperands(expr.operator, left, right);
+                if ((double) right == 0.0) {
+                    throw new RuntimeError(expr.operator, "Deviding zero.");
+                }
                 return (double) left / (double) right;
             }
             case STAR -> {
