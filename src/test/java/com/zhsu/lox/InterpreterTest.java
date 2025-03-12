51a1736028a6ab8b94791a08da5555469b2f8e7a
zhsu <szh12345szh@163.com>
2025-03-12 22:21:09 +0800

    add closure for function

--- src/test/java/com/zhsu/lox/InterpreterTest.java
+++ src/test/java/com/zhsu/lox/InterpreterTest.java
@@ -9,14 +9,19 @@ public class InterpreterTest {
     @Test
     public void testInterpreter() {
         String input = """
-fun fib(n) {
-  if (n <= 1) return n;
-  return fib(n - 2) + fib(n - 1);
+fun makeCounter() {
+  var i = 0;
+  fun count() {
+    i = i + 1;
+    print i;
+  }
+
+  return count;
 }
 
-for (var i = 0; i < 20; i = i + 1) {
-  print fib(i);
-}
+var counter = makeCounter();
+counter(); 
+counter(); 
             """;
 
         Scanner scanner = new Scanner(input);
